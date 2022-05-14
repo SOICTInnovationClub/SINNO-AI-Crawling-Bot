1d29254eafb13a3cfa91d06e6008b9caa9f66da3
Pham Son <phamtienson02@gmail.com>
2022-05-14 23:30:54 +0700

    scraper now writes titles and links to txt file

--- scr/reddit-scraper/main.py
+++ scr/reddit-scraper/main.py
@@ -1,13 +1,21 @@
 import praw
+import datetime
 
 reddit = praw.Reddit()
 
 subreddit = reddit.subreddit('technology')
 
-hot_news = subreddit.hot(limit=10)
+hot_news = subreddit.hot(limit=20)
 
-for submission in hot_news:
-    if not submission.stickied:
-        print(submission.title)
-        print(submission.url)
-        print('\n')
+date = datetime.datetime.now()
+
+filename = f'{date.day}-{date.month}-{date.year}-{date.hour}.txt'
+
+with open(f'./data/{filename}', 'w') as f:
+    for submission in hot_news:
+        if not submission.stickied:
+            f.write(f'{submission.title}\n')
+            f.write(f'{submission.url}\n')
+
+
+
