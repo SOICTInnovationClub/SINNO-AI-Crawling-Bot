079f4bd42fd4c331baee5773db656903191e9d06
Pham Son <phamtienson02@gmail.com>
2022-05-14 23:14:35 +0700

    basic scraper that prints tilte and link to hottest news on r/technology

--- scr/reddit-scraper/main.py
+++ scr/reddit-scraper/main.py
@@ -1,1 +1,13 @@
 import praw
+
+reddit = praw.Reddit()
+
+subreddit = reddit.subreddit('technology')
+
+hot_news = subreddit.hot(limit=10)
+
+for submission in hot_news:
+    if not submission.stickied:
+        print(submission.title)
+        print(submission.url)
+        print('\n')
