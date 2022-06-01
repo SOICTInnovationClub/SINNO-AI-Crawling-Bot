d41e27672e83b6132a417f4d0f082a24d2114661
Pham Son <phamtienson02@gmail.com>
2022-06-01 10:05:42 +0700

    update meta description

--- src/newsproject/newsproject/settings.py
+++ src/newsproject/newsproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4qp_)x2(1y509@yqb=jx=-2ct#q&q&#38zqvh3)oq)qv_73$%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['103.3.60.235']
+ALLOWED_HOSTS = []
 
 
 # Application definition
